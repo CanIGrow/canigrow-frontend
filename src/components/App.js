bc9fdf2c3fac9c49b9a720cf5260c8bf9273c227
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-05 10:33:56 -0400

    Wireframe components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Header from './Header.js';
+import Footer from './Footer.js';
+import Homepage from './Homepage.js';
+import Login from './Login.js';
+import Plantpage from './Plantpage.js';
+import Register from './Register.js';
+import Userpage from './Userpage.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app-body">
+
       </div>
     );
   }
---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+export default class Homepage extends Component {
+  render() {
+    return (
+      <div className="homepage-container">
+      </div>
+    );
+  }
+}
