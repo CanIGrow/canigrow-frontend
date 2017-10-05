9b3029a38dfb2705b3f1fdea1ed5546daacb25ee
Kevin Harrison <kharrison7@elon.edu>
2017-10-05 15:46:22 -0400

    This puts some sample user names from a reducer file, passes them to a combineRducer file, saves that as allReducers in a store in App.js, passes that data to UserList as props and is then displayed.

--- src/components/App.js
+++ src/components/App.js
@@ -11,27 +11,53 @@ import BaseLayout from './Base-Layout.js';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import allReducers from '../reducers/indexReducer.js';
+// This is a container to test with.
+import UserList from '../containers/user-list.js';
+
 
 // This creates a store to hold app state data for redux.
 const store = createStore(
+    // This pulls data from the combined reducer into the store.
     allReducers,
     // applyMiddleware(thunk, promise, logger)
 );
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      username: "testing username",
+      password:"",
+      user_id: null,
+      bio:"",
+      style_number: 0,
+      token: null
+    };
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <BaseLayout>
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route path="/plants/:plant" component={Plantpage} />
-            <Route path="/user/:user" component={Userpage} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-          </Switch>
-        </BaseLayout>
-      </BrowserRouter>
+      // Provides store data to all subcomponents
+      <Provider store={store}>
+        <div className="AppInsideProvider">
+          <BrowserRouter>
+            <BaseLayout>
+              {/* The following is just to test redux. */}
+              <UserList />
+              <Switch>
+                <Route exact path="/" component={Homepage} />
+                <Route path="/plants/:plant" render={(props) => ( <Plantpage username={this.state.username}/> )}/>
+                {/* <Route path="/plants/:plant" component={Plantpage} /> */}
+                <Route path="/user/:user" component={Userpage} />
+                <Route path="/login" render={(props) => ( <Login username={this.state.username}/> )}/>
+                {/* <Route path="/login" component={Login} /> */}
+                <Route path="/register" component={Register} />
+              </Switch>
+            </BaseLayout>
+          </BrowserRouter>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,12 @@ import '../styles/App.css';
 
 export default class Login extends Component {
   render() {
+    console.log('login page render');
+    console.log(this.props);
+    console.log(this.props.username);
     return (
       <div className="login-container">
+        {/* <UserList /> */}
       </div>
     );
   }
