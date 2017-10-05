153d0595af19ae76a17f10fdacf540e92e6cc047
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-05 14:59:25 -0400

    Add breakpoints and add canigrow search

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,9 +5,15 @@ export default class Homepage extends Component {
   render() {
     return (
       <div className="homepage-container main-component-container">
-        <div className="homepage-search-container container">
-          <h2>Can I Grow</h2>
-        </div>
+        <form>
+          <div className="homepage-search-container container">
+            <div className="pagination-centered">
+              <h2 className="homepage-search-title">Can I Grow
+              <input type="search" className="homepage-search-box"/>
+              </h2>
+            </div>
+          </div>
+        </form>
         <h1>This is a Homepage</h1>
       </div>
     );
