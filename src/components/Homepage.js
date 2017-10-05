aa8d205c08cfde7a4cb053f35d496e52fe8f44aa
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-05 15:08:07 -0400

    add state control to form

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 
 export default class Homepage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchbartext: ""
+    }
+  }
+  submitForm = (event) => {
+    event.preventDefault();
+  }
+  handleTextChange = (event) => {
+    event.preventDefault();
+    if (this.state[event.target.id] !== undefined){
+      this.setState({[event.target.id]: event.target.value , fireRedirect: false}, ()=>{
+        //Here is an example that does the callback function after setstate is done
+      });
+    }
+  }
   render() {
     return (
       <div className="homepage-container main-component-container">
-        <form>
+        <form onSubmit={this.submitForm}>
           <div className="homepage-search-container container">
             <div className="pagination-centered">
-              <h2 className="homepage-search-title">Can I Grow
-              <input type="search" className="homepage-search-box"/>
+              <h2 className="homepage-search-title">Can I Grow...
+              <input type="search" id="searchbartext"
+                value={this.state.searchbartext}
+                onChange={this.handleTextChange}
+                className="homepage-search-box"/>
               </h2>
             </div>
           </div>
