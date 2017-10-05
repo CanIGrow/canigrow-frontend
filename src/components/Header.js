b3538b07a47dc842cb45cb86836327439e5af6d9
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-05 13:27:54 -0400

    add working but empty modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,19 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header-container">
-        <button type="button"
-          className="header-hamburger"
-          data-toggle="modal"
-          data-target="#myModal">
+        <button className="header-hamburger"
+        data-toggle="modal" data-target="#hamburger-menu">
           &#9776;
         </button>
+        <div className="container">
+        	<div className="modal left fade in" id="hamburger-menu" tabIndex="-1" >
+        		<div className="modal-dialog">
+        			<div className="modal-content">
+        			<h4>Left Sidebar</h4>
+        			</div>
+        		</div>
+        	</div>
+        </div>
       </div>
     );
   }
