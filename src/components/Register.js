28867573fe9f6fbd43c2bc898fb02f46d38cb7e0
Kevin Harrison <kharrison7@elon.edu>
2017-10-07 17:23:03 -0400

    This adds a basic registration form.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,117 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import { Link, NavLink, Redirect } from 'react-router-dom';
+import request from 'superagent';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-export default class Register extends Component {
+
+export default class Registration extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: '',
+      password: '',
+      bio: '',
+      comments: []
+    };
+  }
+
+  // from: https://github.com/tiycnd/library-frontend/blob/master/src/components/LoginRegister.js
+  updateFromField(stateKey) {
+      return (event) => {
+        this.setState({[stateKey]: event.target.value});
+      }
+    }
+
+  register(event) {
+     let setToken = this.props.setToken;
+     event.preventDefault();
+     request
+        .post("https://pure-spire-67730.herokuapp.com/users")
+        .send(
+         {
+          user: {
+                username: this.state.username,
+                password: this.state.password,
+                bio: this.state.bio
+                }
+             }
+            )
+        .end((err, res) => {
+         if (err) {
+           this.setState({error: res.body.errors.username});
+           console.log("error");
+           console.log(res.body.errors.password);
+           console.log(res.body.errors.username);
+          } else {
+          console.log("registered");
+          // This logs the user in if registration was successful.
+          let setToken = this.props.setToken;
+          request
+          .post("https://pure-spire-67730.herokuapp.com/users/login")
+          .send({username: this.state.username, password: this.state.password})
+          .end((err, res) => {
+            if (err) {
+              this.setState({error: res.body.error});
+              console.log("error");
+            } else {
+              setToken(res.body.token, this.state.username, res.body.user_id);
+              console.log("token returned");
+              console.log(res.body);
+              console.log(res.body.token);
+              console.log(res.body.user_id);
+              console.log(this.state.username);
+            }
+          })
+          }
+        })
+   }
+
   render() {
-    return (
-      <div className="register-container main-component-container">
-        <h1>This is the Register page</h1>
+    let registerContents = null;
+    if (this.props.token) {
+      registerContents =
+      <div className="centerHomeButton">
+        <NavLink className="btn btn-primary btn-lg" type="submit" activeClassName="selected" to="/">
+          <div>Registration Successful!</div>
+          <div>You are now logged in as: {this.props.username}</div>
+          <span>Go to the Homepage</span>
+        </NavLink>
       </div>
-    );
+    } else {
+      registerContents =
+        <div className="container-fluid">
+            <div className="card">
+              {this.state.error && <div className="alert">Username {this.state.error}</div>}
+              <div className="card-block">
+                <h3>Registration Form:</h3>
+                <form className="enterForm" onSubmit={this.handleFormSubmit}>
+                  <div className="form-group">
+                    <h6>User Name:</h6>
+                    <input type="username" onChange={this.updateFromField('username')} value={this.state.username} placeholder="User Name"/>
+                  </div>
+                  <div className="form-group">
+                    <h6>Password:</h6>
+                    <input type="password" onChange={this.updateFromField('password')} value={this.state.password} placeholder="********"/>
+                  </div>
+                  <div className="form-group">
+                    <h6>Personal Bio:</h6>
+                    <textarea type="text" onChange={this.updateFromField('bio')} value={this.state.bio} id='wmd-input' className='wmd-input processed' name='post-text' cols='50' rows='5' tabIndex='101' data-min-length placeholder='Tell Us About Yourself'></textarea>
+                  </div>
+                  <div className="form-group pull-right">
+                    <button className="btn btn-primary btn-lg" type="submit" onClick={event => this.register(event)}>Register</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+        </div>
+    }
+
+
+    return (
+      <div>
+      {registerContents}
+      </div>
+    )
   }
 }
