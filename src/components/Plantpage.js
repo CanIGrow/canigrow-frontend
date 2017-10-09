ecabf09365c9d139731b68f8279c6659744fbe86
Kevin Harrison <kharrison7@elon.edu>
2017-10-09 16:59:18 -0400

    This gets plant data for the plant/:id page.

--- src/components/Plantpage.js
+++ src/components/Plantpage.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
+import request from 'superagent';
 import '../styles/App.css';
 
 export default class Plantpage extends Component {
+
+
+  plantInfoGet(event) {
+    //  This lets the user 'bypass' CORs via proxy.
+     const proxyurl = "https://boiling-castle-73930.herokuapp.com/";
+     let plant_id = 1;
+    //  event.preventDefault();
+     request
+      .get(`${proxyurl}https://canigrow.herokuapp.com/api/plants/${plant_id}`)
+      .end((err, res)=>{
+        if (res !== undefined){
+          console.log(res.body);
+        }
+      })
+  }
+
+  componentWillMount() {
+    console.log(this.props);
+    this.plantInfoGet();
+  }
+
+
   render() {
     return (
       <div className="plantpage-container main-component-container">
