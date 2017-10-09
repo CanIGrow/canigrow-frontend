1f95cf3eef9056d126e32cf27bd04de7dfa05b2d
Kevin Harrison <kharrison7@elon.edu>
2017-10-09 17:24:45 -0400

    This updates plant page.

--- src/components/Plantpage.js
+++ src/components/Plantpage.js
@@ -3,15 +3,19 @@ import request from 'superagent';
 import '../styles/App.css';
 
 export default class Plantpage extends Component {
-
+  constructor(props) {
+      super(props)
+      this.state = {
+        plant_id: 1,
+      };
+  }
 
   plantInfoGet(event) {
     //  This lets the user 'bypass' CORs via proxy.
      const proxyurl = "https://boiling-castle-73930.herokuapp.com/";
-     let plant_id = 1;
-    //  event.preventDefault();
+     event.preventDefault();
      request
-      .get(`${proxyurl}https://canigrow.herokuapp.com/api/plants/${plant_id}`)
+      .get(`${proxyurl}https://canigrow.herokuapp.com/api/plants/${this.state.plant_id}`)
       .end((err, res)=>{
         if (res !== undefined){
           console.log(res.body);
@@ -21,14 +25,31 @@ export default class Plantpage extends Component {
 
   componentWillMount() {
     console.log(this.props);
-    this.plantInfoGet();
   }
 
+  updateFromField(stateKey) {
+      return (event) => {
+        this.setState({[stateKey]: event.target.value});
+      }
+  }
 
   render() {
     return (
       <div className="plantpage-container main-component-container">
-        <h1>This is a Plantpage</h1>
+        <p>This is a Plantpage</p>
+        <form className="enterForm" onSubmit={this.handleFormSubmit}>
+          <div className="form-group">
+            <h6>Plant ID:</h6>
+            <input type="text" onChange={this.updateFromField('plant_id')} value={this.state.plant_id} placeholder="plant_id"/>
+          </div>
+          <div className="form-group pull-right">
+            <button className="btn btn-primary btn-lg" type="submit" onClick={event => this.plantInfoGet(event)}>Get Plant Information</button>
+          </div>
+        </form>
+
+        <img className="plant_big_image" src="" alt="plant_img"/>
+
+
       </div>
     );
   }
