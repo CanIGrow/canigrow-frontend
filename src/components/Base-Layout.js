5a3941d7892ba50c1be4af0c9efd940aed5da664
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-25 11:42:26 -0400

    change background to grey

--- src/components/Base-Layout.js
+++ src/components/Base-Layout.js
@@ -67,9 +67,9 @@ export default class BaseLayout extends Component {
     }
     else {
       outerStyle = {
-          // "backgroundColor": "#D3D3D3",
+          "backgroundColor": "#D3D3D3",
           // "backgroundImage": 'url(https://i.imgur.com/fW194uY.jpg)',
-          "backgroundImage": 'url(http://www.loversiq.com/daut/as/f/f/flowers-nature-plant-beautiful-green-red-yellow-pink-blue-1811457_beautiful-plants_home-decor_target-home-decor-decorating-blogs-inexpensive-fall-websites-walmart-pinterest-decorator.jpg)',
+          // "backgroundImage": 'url(http://www.loversiq.com/daut/as/f/f/flowers-nature-plant-beautiful-green-red-yellow-pink-blue-1811457_beautiful-plants_home-decor_target-home-decor-decorating-blogs-inexpensive-fall-websites-walmart-pinterest-decorator.jpg)',
           "width": "100%",
           "height": "100%",
           // "-webkit-background-size": "cover",
